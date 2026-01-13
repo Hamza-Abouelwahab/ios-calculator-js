ac79712c34f727b761ec0bc3b83c94b881eddb0c
hamza-133 <hamzabouelwahab04@gmail.com>
2026-01-13 16:42:59 +0100

    fix : handle case if user write some think not a number alert error

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,23 @@
-let inputResult = document.querySelector("#input-result")
-    
+let inputResult = document.querySelector("#input-result");
+
 function appendtoInput(input) {
-    inputResult.value += input 
+  inputResult.value += input;
 }
 function cleartoInput() {
-    inputResult.value = ""; 
+  inputResult.value = "";
 }
+
+function deletedtoInput() {
+    inputResult.value = inputResult.value.slice(0,-1)
+}
+
 function calculator() {
-    try{
-        let expr = inputResult.value.replace(/,/g, '.');
-        let result = eval(expr)
-        inputResult.value = parseFloat(result.toFixed(2))
-    }
-    catch(error){
-        inputResult.value = "error"
-    }
+  try {
+    let expr = inputResult.value.replace(/,/g, ".");
+    expr = expr.replace(/(\d+(\.\d+)?)%/g,"($1/100)")
+    let result = eval(expr);
+    inputResult.value = parseFloat(result.toFixed(2));
+  } catch (error) {
+    inputResult.value = "error";
+  }
 }
